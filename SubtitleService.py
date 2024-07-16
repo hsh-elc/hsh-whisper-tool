c7cf84450a08f11e6c5163a4c141663fb109e844
Firehawk1008 <jan-pierre.wesche@stud.hs-hannover.de>
2024-07-16 12:42:58 +0200

    finished

--- SubtitleService.py
+++ SubtitleService.py
@@ -1,31 +1,26 @@
 import os
-from whisperBackend import Translator
-from whisperBackend import FileCreater
+from whisperBackend import Transcriber
 
 
 class SubtitleService:
     def __init__(self) -> None:
         self.file_path = None
-        self.languages = None
         self.filetype = None
-        self.translator = Translator()
+        self.translator = Transcriber()
         self.bulk = False
 
     def create_subtitles(
-        self, file_path: str, languages: list[str], filetypes: list[str], bulk: bool = False
+        self, file_path: str, filetypes: list[str], bulk: bool = False
     ) -> None:
         # Create subtitles for a given file.
 
         # Args:
         #     file_path (str): The path of the audio/video file.
-        #     languages (list[str]): The list of languages to translate the subtitles into.
         #     filetypes (list[str]): The list of file extensions for the output subtitles.
         #     bulk (bool, optional): Flag indicating whether to process multiple files in bulk. Defaults to False.
         self.file_path = file_path
-        self.languages = languages
         self.filetype = filetypes
-        self.translator = Translator()
-        self.file_creater = FileCreater()
+        self.translator = Transcriber()
         self.bulk = bulk
 
         if bulk:
@@ -36,24 +31,7 @@ class SubtitleService:
             path = (os.path.split(file_path)[0])
             for file in files:
                 file_path = os.path.join(path, file)
-                for language in self.languages:
-                    # Translate the audio file to the specified language
-                    transcription_response = self.translator.translate(file_path, str(language))
-                    
-                    # Create a file with the specified file extensions for each language
-                    for extension in filetypes:
-                        self.file_creater.format_writer(
-                            file_path, extension, transcription_response, language
-                        )
+                transcription_response = self.translator.transcribe(file_path, filetypes)
             return
 
-        # Transcribe and translate the video and create the output files with the specified file extensions
-        for language in self.languages:
-            # Translate the video to the specified language
-            transcription_response = self.translator.translate(self.file_path, str(language))
-            
-            # Create a file with the specified file extensions for each language
-            for extension in filetypes:
-                self.file_creater.format_writer(
-                    file_path, extension, transcription_response, language
-                )
+        transcription_response = self.translator.transcribe(self.file_path, filetypes)
--- whisperBackend.py
+++ whisperBackend.py
@@ -1,23 +1,59 @@
-from datetime import timedelta
 import os
-import whisper
+from numpy import unicode_
+import stable_whisper
 import Constants
+from faster_whisper import WhisperModel
+import torch
+import math
 
-class FileCreater:
-    # takes the transcription and writes it into a subtitle format file, only formats implemented by whisper work
-    def format_writer(self, filepath: str, output_extension, transcription_response, file_language_name = ""):
-        path, file_name = os.path.split(filepath)
-        output_filename = file_name.split(".")[0] + "_" + file_language_name + "." + output_extension
-        output_writer = whisper.utils.get_writer(output_extension, path)
-        output_writer(transcription_response, output_filename)
+
+def convert_seconds_to_hms(seconds):
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    milliseconds = math.floor((seconds % 1) * 1000)
+    output = f"{int(hours):02}:{int(minutes):02}:{int(seconds):02},{milliseconds:03}"
+    return output
 
 
 # Translates given and also transcribes at the same time 
-class Translator:
+class Transcriber:
     def __init__(self) -> None:
-        self.model = whisper.load_model(Constants.model_size)
+        os.environ['KMP_DUPLICATE_LIB_OK']='True'
+        torch.cuda.is_available()
+        self.model = stable_whisper.load_faster_whisper(Constants.model_size)
         
     # Transcribes and Translates the given file (Audio or Video)
-    def translate(self, filepath: str, language: str)-> dict[str, str | list]:
-        translate_response = self.model.transcribe(filepath, language = language, verbose = False)
-        return translate_response
+    def transcribe(self, filepath: str, filetypes)-> dict[str, str | list]:
+        self.segments, info = self.model.transcribe(filepath)
+        segmentList = list(self.segments)
+        for filetype in filetypes:
+
+            if filetype == "srt":
+                self.createSrt(filepath, segmentList)
+
+            if filetype == "vtt":
+                self.createVtt(filepath, segmentList)
+                
+
+    def createSrt(self, filepath: str, segments):
+        count = 0
+        with open(filepath.split(".")[0] + ".srt", 'w') as f:  # Open file for writing
+            for segment in segments:
+                count +=1
+                duration = f"{convert_seconds_to_hms(segment.start)} --> {convert_seconds_to_hms(segment.end)}\n"
+                text = f"{segment.text.lstrip()}\n\n"
+
+                f.write(f"{count}\n{duration}{text}")  # Write formatted string to the file
+                print(f"{duration}{text}",end='')
+            f.close()
+
+    def createVtt(self, filepath: str, segments):
+        with open(filepath.split(".")[0] + ".vtt", 'w') as f:  # Open file for writing
+            f.write("WEBVTT\n\n")
+            for segment in segments:
+                duration = f"{convert_seconds_to_hms(segment.start)} --> {convert_seconds_to_hms(segment.end)}\n"
+                text = f"{segment.text.lstrip()}\n\n"
+
+                f.write(f"{duration}{text}")  # Write formatted string to the file
+                print(f"{duration}{text}",end='')
+            f.close()
